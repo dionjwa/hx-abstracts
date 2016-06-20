e900bae33e04d149a9df8d2c33680023000e464c
Dion Amago <dion@transition9.com>
2016-06-20 14:13:51 -0700

    Extra test

--- test/src/tests/Time.hx
+++ test/src/tests/Time.hx
@@ -7,8 +7,6 @@ class Time extends haxe.unit.TestCase
 	public function testTime()
 	{
 		var foo :DummyFloatAbstract = 2000.0;
-		// var bar :Milliseconds = foo;
-
 
 		var timeValue = 2000.0;
 		var ms = new Milliseconds(timeValue);
@@ -31,6 +29,27 @@ class Time extends haxe.unit.TestCase
 		assertEquals(deltaMinutes.toFloat(), delta / 1000 / 60);
 
 		assertEquals((new Minutes(10) % new Minutes(3)).toFloat(), 1.0);
+	}
+
+	public function testTime2()
+	{
+		var nowFloat = Date.now().getTime();
+		var now = new Milliseconds(nowFloat);
+		var ts = new TimeStamp(now);
+		assertEquals(ts.toString(), Date.fromTime(now.toFloat()).toString());
+		assertEquals(ts.toFloat(), nowFloat);
+
+		var ms1 = new Milliseconds(20);
+		var ms2 = new Milliseconds(5);
+		var ms3 = ms1 - ms2;
+		assertEquals(ms3.toFloat(), 15.0);
+
+		var seconds = new Seconds(10);
+		var ms = seconds.toMilliseconds().toFloat();
+		var timePlus :TimeStamp = ts.addSeconds(seconds);
+		var floatTimePlus = nowFloat + ms;
+		var timePlusFloat :Float = timePlus.toFloat();
+		assertEquals(timePlusFloat, floatTimePlus);
 	}
 
 	static function iAcceptFloats(_ :Float)
